eb4249e3098f17dc06ecc4158b56114c136bacc7
Ahmed Fawzi <ahmedfawzi636@gmail.com>
2025-10-27 14:42:54 +0300

    Add Discount service setup and proto

---
+++ DiscountServices/Discount/Extensions/IServiceCollectionExtension.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Discount.Extensions;
+
+public static class IServiceCollectionExtension
+{
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddGrpc();
+
+        return services;
+    }
+}
